1cc8f1bb117f5d8023c6202cd8d3adc38d17e08c
Ramon <ramonmelo.com@gmail.com>
2024-03-15 14:35:19 -0300

    implementação função show_struct e chamada na função main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 struct Vector {
     x:i32,
     y:i32,
@@ -5,6 +7,22 @@ struct Vector {
 }
 
 fn main() {
+
+
+    println!("what is your name?");
+
+    let mut name = String::new(); // instanciação do de uma String
+    io::stdin().read_line(&mut name)
+    .expect("Failed to read line");
+
+    println!("Hello!{}",name);
+
+    show_struct(); // chamada da função show_struct
+
+
+}
+
+fn show_struct() {
     let v1: Vector =  Vector {x:1, y:2,z:3};
     println!("{{{},{},{}}}",v1.x, v1.y,v1.z);
 }
